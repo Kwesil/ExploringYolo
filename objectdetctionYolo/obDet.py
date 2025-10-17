049589d6e3ebcadac38ec3eee6f0aff373d25206
Kwesil <nwabikejr@gmail.com>
2025-10-17 13:22:05 +0100

    updated to detect from live webcam

--- objectdetctionYolo/obDet.py
+++ objectdetctionYolo/obDet.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.pt')
 
 # Run inference on the source
-results = model(source="gymnasts.mp4", show=True, conf=0.4, save=True) # generator of 
+results = model(source=0, show=True, conf=0.4, save=True) # generator of 
